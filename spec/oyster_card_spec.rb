f3892b7531b179f305b5dfb6a8e995c369c8e2fb
Benjamin <ben.smith.gordon@gmail.com>
2018-06-18 15:08:09 +0100

    adding a balance

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,3 +1,10 @@
-describe OysterCard do
+require "oyster_card.rb"
 
-end
+ describe OysterCard do
+
+  describe "balance" do
+    it "should start with a balance of 0" do
+      expect(OysterCard.new.balance).to eq 0
+      end
+    end
+  end
